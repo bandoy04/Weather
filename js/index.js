f26666814594722a92215166234e08d5ff073125
bandoy04 <bandoy04@gmail.com>
2016-10-19 22:34:37 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,9 @@ $(document).ready(function() {
     setInterval(getWeather, 600000); //Update the weather every 10 minutes.
 });
 
-// var d = new Date();
-// document.getElementById("currentDate").innerHTML = d;
-// 22721202
-
 function getWeather() {
     $.simpleWeather({
-        woeid: '1096823', 
+        woeid: '1096823', // WOEID Location
         location: '',
         unit: 'c',
         success: function(weather) {
@@ -18,15 +14,13 @@ function getWeather() {
             html = '<div class="weatherimage"><img src="' + weather.image + '"> </div>'
             html += '<div class="currentweather">' + weather.temp + '&deg;' + weather.units.temp + '</div>';
             html += '<div class="currentDate">' + formattedDate + '</div>';
-           
             html += '<div class="forecast">'
 
             for (var i = 0; i < 5; i++) {
                 html += '<div class="forecast' + [i] + '"><img src="' + weather.forecast[i].thumbnail + '"> <br>' + weather.forecast[i].day + ' - ' + weather.forecast[i].text + '<br> High: ' + weather.forecast[i].high + '&deg;C <br> Low: ' + weather.forecast[i].low + '&deg;C <br> </div>';
             }
-            
-            html += '</div>'
 
+            html += '</div>'
 
             $("#mainDiv").html(html);
         },
@@ -35,42 +29,3 @@ function getWeather() {
         }
     });
 }
-
-
-/*
-           for (var i = 0; i < 4; i++) {
-                html += '<div class="forecast' + [i] + '"><img src="' + weather.forecast[i].thumbnail + '"> <br>' + weather.forecast[i].day + ' - ' + weather.forecast[i].text + '<br> High: ' + weather.forecast[i].high + '&deg;C <br> Low: ' + weather.forecast[i].low + '&deg;C <br> </div>';
-            }
-*/
-
-
-/*
-function getWeather() {
-    $.simpleWeather({
-        woeid: '1096823', 
-        location: '',
-        unit: 'c',
-        success: function(weather) {
-            var newDate = new Date();
-            var formattedDate = newDate.format("dddd, mmmm dS");
-            html = '<div class="weatherimage"><img src="' + weather.image + '"> </div>'
-            html += '<div class="currentweather"><h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2> </div>';
-            html += '<div class="currentDate">' + formattedDate + '</div>';
-            
-            //       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-            //       html += '<li class="currently">'+weather.currently+'</li></div>';
-
-            html += '<div class="forecast">'
-            for (var i = 0; i < 4; i++) {
-                html += '<div class="'+[i]+'"><img src="' + weather.forecast[i].thumbnail + '"> <br>' + weather.forecast[i].day + ' - ' + weather.forecast[i].text + '<br> High: ' + weather.forecast[i].high + '&deg;C <br> Low: ' + weather.forecast[i].low + '&deg;C <br> </div>';
-            }
-            html += '</div>'
-
-            $("#weather").html(html);
-        },
-        error: function(error) {
-            $("#weather").html('<p>' + error + '</p>');
-        }
-    });
-}
-*/
